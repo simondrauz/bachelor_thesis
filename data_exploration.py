1c0bb02474fa5cf1970e3f5de2357508e464184f
Simon Drauz <s.drauz@gmail.com>
2023-08-14 11:46:23 +0200

    adds function to calculate correlation between a list of target variables and a list of input variables

--- data_exploration.py
+++ data_exploration.py
@@ -1,27 +1,30 @@
-
-import os
 import pandas as pd
 
-output_directory = r"C:\Users\Uwe Drauz\Documents\bachelor_thesis_local\personal_competition_data\Baseline_Model"
 
-crps_scores_all_year_country_specific = {}
-crps_scores_all_year_global = {}
-for year in [2015, 2016, 2017, 2018, 2019]:
-    file_path = rf'C:\Users\Uwe Drauz\Documents\bachelor_thesis_local\personal_competition_data\Baseline_Model\crps_scores_all_year_{year}_country_specific.parquet'
-    crps_scores_all_year_country_specific[year] = pd.read_parquet(file_path)
-    file_path = rf'C:\Users\Uwe Drauz\Documents\bachelor_thesis_local\personal_competition_data\Baseline_Model\crps_scores_all_year_{year}_global.parquet'
-    crps_scores_all_year_global[year] = pd.read_parquet(file_path)
+def calculate_correlation(data: pd.DataFrame, target_variables: list, input_variables: list):
+    """
+    Calculate the correlation between a list of target variables and a list of input variables in a pandas DataFrame.
 
-crps_scores_all_year_country_specific_year_2015 = crps_scores_all_year_country_specific[2015]
-crps_scores_all_year_country_specific_year_2016 = crps_scores_all_year_country_specific[2016]
-crps_scores_all_year_country_specific_year_2017 = crps_scores_all_year_country_specific[2017]
-crps_scores_all_year_country_specific_year_2018 = crps_scores_all_year_country_specific[2018]
-crps_scores_all_year_country_specific_year_2019 = crps_scores_all_year_country_specific[2019]
+    Parameters:
+        data (pandas.DataFrame): The DataFrame containing the variables.
+        target_variables (list): A list of target variable names.
+        input_variables (list): A list of input variable names.
 
-crps_scores_all_year_global_year_2015 = crps_scores_all_year_global[2015]
-crps_scores_all_year_global_year_2016 = crps_scores_all_year_global[2016]
-crps_scores_all_year_global_year_2017 = crps_scores_all_year_global[2017]
-crps_scores_all_year_global_year_2018 = crps_scores_all_year_global[2018]
-crps_scores_all_year_global_year_2019 = crps_scores_all_year_global[2019]
+    Returns:
+        pandas.DataFrame: A DataFrame containing the correlation values.
+    """
+    # Select the target variables and input variables from the DataFrame
+    variables = target_variables + input_variables
+    df = data[variables]
 
-print('fin')
+    # Calculate the correlation matrix
+    correlation_matrix = df.corr()
+
+    # Extract the correlation values of the target variables
+    correlation_values = correlation_matrix.loc[target_variables]
+
+    # Remove the target variables from the correlation values
+    correlation_values = correlation_values.drop(columns=target_variables)
+
+    return correlation_values
+
