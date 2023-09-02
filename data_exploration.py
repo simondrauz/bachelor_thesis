55c1fa2dbfeb90e40d3411fc7a4ec3648817c0ad
Simon Drauz <s.drauz@gmail.com>
2023-09-02 10:56:37 +0200

    adds function to calculate data characteristics of a dataframe for logging in mlflow

--- data_exploration.py
+++ data_exploration.py
@@ -28,3 +28,45 @@ def calculate_correlation(data: pd.DataFrame, target_variables: list, input_vari
 
     return correlation_values
 
+
+def calculate_data_characteristics(data: pd.DataFrame) -> dict:
+    """
+    Calculates data characteristics.
+
+    Args:
+        data: The dataset in a Pandas DataFrame format.
+
+    Returns:
+        A dictionary containing the data characteristics.
+    """
+
+    # Define the metrics to be computed for each column
+    metrics = ['min', '25_percentile', '50_percentile', '75_percentile', 'max', 'mean', 'std']
+
+    characteristics = {}
+
+    # Stats for the whole dataframe
+    characteristics['dataframe_info'] = {
+        "n_samples": data.shape[0],
+        "n_regressors": data.shape[1] - 1  # Exclude the target variable
+    }
+
+    # Iterate over each column to compute statistics
+    for column in data.columns:
+        column_describe = data[column].describe(percentiles=[.25, .5, .75])
+
+        # Convert Series to dictionary and replace '%' with 'percentile'
+        column_describe_dict = column_describe.to_dict()
+        column_describe_dict = {k.replace('%', 'percentile'): v for k, v in column_describe_dict.items()}
+
+        column_stats = {}
+        for metric in metrics:
+            value = column_describe_dict.get(metric, None)
+            if value is not None:
+                value = round(value, 4)
+            column_stats[metric] = value
+
+        # Add column stats to the main dictionary
+        characteristics[column] = column_stats
+
+    return characteristics
