dd8df476ace20e5ebff257d311b87fa1c4120779
Simon Drauz <s.drauz@gmail.com>
2023-09-08 11:34:08 +0200

    (feat) introduce "server already in use" check for MLFlow server class

--- help_functions.py
+++ help_functions.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import subprocess
 import time
 import mlflow
+import socket
 
 
 def generate_datetime_list(start_year: int, start_month: int, end_year: int, end_month: int) -> list:
@@ -51,22 +52,36 @@ class MLFlowServer:
         self.tracking_uri = tracking_uri
         self.process = None
 
+    def _is_port_in_use(self, port: int) -> bool:
+        """
+        Checks if the given port is already in use.
+        """
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            return s.connect_ex(('localhost', port)) == 0
+
     def start(self):
         """
         Starts the MLflow user interface and sets the tracking URI and experiment.
+        Only starts if the port is not already in use.
         """
-        self.process = subprocess.Popen(["mlflow", "ui"])
+        if not self._is_port_in_use(5000):
+            self.process = subprocess.Popen(["mlflow", "ui"])
+            time.sleep(5)  # give it some time to start
+            mlflow.set_tracking_uri(self.tracking_uri)
+            mlflow.set_experiment(self.experiment_name)
+        else:
+            print("MLflow server is already running or port 5000 is in use.")
 
-        time.sleep(5)
-
-        mlflow.set_tracking_uri(self.tracking_uri)
-        mlflow.set_experiment(self.experiment_name)
-
+# FIxMe: The stop function does not stop the server, consider leaving it running
     def stop(self):
         """
         Shuts down the MLflow server.
         """
         if self.process:
             self.process.terminate()
-            self.process.wait()  # Wait for the process to actually terminate
+            try:
+                self.process.wait(timeout=5)  # Wait for 5 seconds to see if the process terminates
+            except subprocess.TimeoutExpired:
+                print("MLflow server did not terminate gracefully. Forcibly killing.")
+                self.process.kill()
             self.process = None
